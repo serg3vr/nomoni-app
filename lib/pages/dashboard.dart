fce0dc777e9c30c00d3dce82d58a2434243f6eee
Sergio Valdez <sergioavr93@gmail.com>
2020-10-31 16:10:50 -0600

    Mostrar datos dashboard

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -17,7 +17,9 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
 
   List<dynamic> expensesList = [];
+  List<dynamic> categoriesList = [];
   List<dynamic> typesList = [];
+  List<dynamic> paymentMethodsList = [];
   Future future;
   double dailyAmount = 0;
   double monthlyAmount = 0;
@@ -33,6 +35,7 @@ class _DashboardState extends State<Dashboard> {
       bool result = data['result'];
       if (result) {
         monthlyAmount = double.parse(data['amount']);
+        print(monthlyAmount);
       }
     });
     
@@ -43,13 +46,42 @@ class _DashboardState extends State<Dashboard> {
         dailyAmount = double.parse(data['amount']);
       }
     });
-    return api.get('expenses/divided-by-months?pastMonths=5').then((response) {
+
+    // Expenses by categories
+    await api.get('expenses/by-categories').then((response) {
+      Map data = jsonDecode(response.body);
       print(response.body);
+      bool result = data['result'];
+      if (result) {
+        categoriesList = data['expenses'];
+      }
+    });
+
+    // Expenses by types
+    await api.get('expenses/by-types').then((response) {
+      Map data = jsonDecode(response.body);
+      print(response.body);
+      bool result = data['result'];
+      if (result) {
+        typesList = data['expenses'];
+      }
+    });
+
+    // // Expenses by payment methods
+    await api.get('expenses/by-payment-methods').then((response) {
+      Map data = jsonDecode(response.body);
+      print(response.body);
+      bool result = data['result'];
+      if (result) {
+        paymentMethodsList = data['expenses'];
+      }
+    });
+
+    return api.get('expenses/divided-by-months?pastMonths=5').then((response) {
       Map data = jsonDecode(response.body);
       bool result = data['result'];
       if (result) {
         expensesList = data['months'];
-        print(expensesList);
       }
     });
   }
@@ -137,57 +169,13 @@ class _DashboardState extends State<Dashboard> {
                 SizedBox(height: 24,),
                 //////////////////////////////////////////////////
                 //////////////////////////////////////////////////
-                Text(
-                  'Expenses',
-                  // textAlign: TextAlign.center,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    color: Colors.grey[800],
-                    fontSize: 22.0
-                  ),
-                ),
-                Text(
-                  'Overview of Last five months of Expenses',
-                  // textAlign: TextAlign.center,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    color: Colors.grey[400],
-                    fontSize: 16.0
-                  ),
-                ),
-                Container(
-                  height: 372.0,
-                    child: ListView.builder(
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: expensesList.length,
-                      itemBuilder: (context, index) {
-                        String date = expensesList[index]['date'];
-                        // String amount = expensesList[index]['date'];
-                        if (date.length >= 22) {
-                          date = expensesList[index]['date'].substring(0, 22) + '...';
-                        }
-                        return ListTile(
-                          // onTap: () {},
-                          title: Text(date),
-                          trailing: Container(child: Column(
-                            children: <Widget>[
-                              // FlutterLogo(),
-                              Text(
-                                r"$ " + expensesList[index]['amount'].toString(),
-                                textDirection: TextDirection.ltr,
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  color: Colors.black87,
-                                ),
-                              ),
-                            ],
-                          )),
-                        );
-                      }
-                    )
-                  ),
+                lastFiveMonthsWidget(),
+                SizedBox(height: 24,),
+                expensesByCategoriesWidget(),
+                SizedBox(height: 24,),
+                expensesByTypesWidget(),
+                SizedBox(height: 24,),
+                expensesByPaymentMethodsWidget(),
                 RaisedButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/expenses');
@@ -205,4 +193,237 @@ class _DashboardState extends State<Dashboard> {
       }
     );  
   }
+
+  Widget lastFiveMonthsWidget() {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Expenses',
+            // textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              color: Colors.grey[800],
+              fontSize: 22.0
+            ),
+          ),
+          Text(
+            'Overview of Last five months of Expenses',
+            // textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              color: Colors.grey[400],
+              fontSize: 16.0
+            ),
+          ),
+          Container(
+            height: 372.0,
+            child: ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: expensesList.length,
+              itemBuilder: (context, index) {
+                String date = expensesList[index]['date'];
+                // String amount = expensesList[index]['date'];
+                if (date.length >= 22) {
+                  date = expensesList[index]['date'].substring(0, 22) + '...';
+                }
+                return ListTile(
+                  // onTap: () {},
+                  title: Text(date),
+                  trailing: Container(child: Column(
+                    children: <Widget>[
+                      // FlutterLogo(),
+                      Text(
+                        r"$ " + expensesList[index]['amount'].toString(),
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  )),
+                );
+              }
+            )
+          ),
+        ],
+      ),
+    );
+  }
+  
+  Widget expensesByCategoriesWidget() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Expenses by category',
+            textAlign: TextAlign.left,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              color: Colors.grey[800],
+              fontSize: 22.0
+            ),
+          ),
+          Text(
+            'Overview of Expenses by Category',
+            // textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              color: Colors.grey[400],
+              fontSize: 16.0
+            ),
+          ),
+          Container(
+            height: 372.0,
+            child: ListView.builder(
+              // physics: NeverScrollableScrollPhysics(),
+              itemCount: categoriesList.length,
+              itemBuilder: (context, index) {
+                // String date = categoriesList[index]['date'];
+                // if (date.length >= 22) {
+                //   date = categoriesList[index]['date'].substring(0, 22) + '...';
+                // }
+                return ListTile(
+                  // onTap: () {},
+                  title: Text(categoriesList[index]['name']),
+                  trailing: Container(child: Column(
+                    children: <Widget>[
+                      // FlutterLogo(),
+                      Text(
+                        r"$ " + categoriesList[index]['amount'].toString(),
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  )),
+                );
+              }
+            )
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget expensesByTypesWidget() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Expenses by types',
+            // textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              color: Colors.grey[800],
+              fontSize: 22.0
+            ),
+          ),
+          Text(
+            'Overview of Expenses by types',
+            // textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              color: Colors.grey[400],
+              fontSize: 16.0
+            ),
+          ),
+          Container(
+            height: 372.0,
+            child: ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: typesList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  // onTap: () {},
+                  title: Text(typesList[index]['name']),
+                  trailing: Container(child: Column(
+                    children: <Widget>[
+                      // FlutterLogo(),
+                      Text(
+                        r"$ " + typesList[index]['amount'].toString(),
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  )),
+                );
+              }
+            )
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget expensesByPaymentMethodsWidget() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Expenses by payment methods',
+            // textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              color: Colors.grey[800],
+              fontSize: 22.0
+            ),
+          ),
+          Text(
+            'Overview of Expenses by payment methods',
+            // textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              color: Colors.grey[400],
+              fontSize: 16.0
+            ),
+          ),
+          Container(
+            height: 372.0,
+            child: ListView.builder(
+              // physics: NeverScrollableScrollPhysics(),
+              itemCount: paymentMethodsList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  // onTap: () {},
+                  title: Text(paymentMethodsList[index]['name']),
+                  trailing: Container(child: Column(
+                    children: <Widget>[
+                      // FlutterLogo(),
+                      Text(
+                        r"$ " + paymentMethodsList[index]['amount'].toString(),
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  )),
+                );
+              }
+            )
+          ),
+        ],
+      ),
+    );
+  }
+
 }
